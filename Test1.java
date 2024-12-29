26c8558a552d346c298f27a7e6970b8f514571ca
ramprasadsuthi <ramprasadsuthi@gmail.com>
2024-12-29 22:43:17 +0530

    changes in Test1.java

--- Test1.java
+++ Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
         System.out.println("Hello I am Java");
 	System.out.println("Wow, Its working..");
 	System.out.print("My name is Revathi, and I am studying in \"Vignan\" College.");
+	//Test pad
 
      }
 
